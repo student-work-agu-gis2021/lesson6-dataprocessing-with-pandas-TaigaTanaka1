e83605c2e5f43f9e165d8c0d15fd426f93c5841d
TaigaTanaka1 <>
2021-05-22 14:25:48 +0000

    I misunderstood the problem and made a mistake. I fixed it.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -17,12 +17,14 @@ import numpy as np
 data = None
 
 # YOUR CODE HERE 1
+#create the file path
 fp = r'data/1091402.txt'
+#read the file
 data = pd.read_csv(
   fp,
-  skiprows = [1], #２行目スキップ
+  skiprows = [1], #skip the second row
   delim_whitespace=True,
-  na_values = [-9999]
+  na_values = [-9999] #-9999 into NaN
 )
 
 
@@ -36,9 +38,8 @@ data = pd.read_csv(
 
 tavg_nodata_count = None
 #YOUR CODE HERE 2
-#.isnull()でNaNかどうか判定して、それの合計を取る
+#check whether the value exists or not(by isnull() method). Then sum of true is the answer.
 tavg_nodata_count = data['TAVG'].isnull().sum()
-
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -52,7 +53,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 tmin_nodata_count = None
 #YOUR CODE HERE 3
 
-#.isnull()でNaNかどうか判定して、それの合計を取る
+#check whether the value exists or not(by isnull() method). Then sum of true is the answer.
 tmin_nodata_count = data['TMIN'].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
@@ -66,10 +67,8 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
-#'DATE'の列の長さ
+#Just check the length of 'data'
 day_count = len(data['DATE'])
-
-
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -83,7 +82,7 @@ print("Number of days:", day_count)
 first_obs = None
  
 # YOUR CODE HERE 5
-#一つ目のデータを見れば良い
+# the first row of the column 'DATE' is the answer
 first_obs = data.loc[0, 'DATE']
 
 
@@ -98,6 +97,7 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
+#the last row of the column 'DATE' is the answer.
 last_obs = data.loc[day_count-1, 'DATE']
 
 
@@ -106,17 +106,14 @@ last_obs = data.loc[day_count-1, 'DATE']
 print('Date of the last observation:', last_obs)
 #CAUTION!!! DON'T EDIT THIS PART END
 
-
 # - Find the average temperature for the whole data file (all observtions) from column `TAVG`
 #     - Assign your answer into a variable called `avg_temp`
 
 avg_temp = None
 
 # YOUR CODE HERE 7
+#calculate the average of temperature
 avg_temp = data['TAVG'].mean()
-
-
-
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -130,7 +127,7 @@ print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
 avg_temp_1969 = None
 
 # YOUR CODE HERE 8
-#'DATE'が19690501以上19690901未満の範囲を指定して、平均を取る
+# Calculate the mean of data that fulfill this requirements -> 19690501 <= data['DATE'] < 19690901
 avg_temp_1969 = data['TMAX'].loc[(data['DATE'] >= 19690501) & (data['DATE'] < 19690901)].mean()
 
 
@@ -141,62 +138,38 @@ print('Average temperature (F) for the Summer of 69:', round(avg_temp_1969, 2))
 
 
 # ## Problem 2 - Calculating monthly average temperatures (*3 points*)
-# 
 
 monthly_data = None
 
 # YOUR CODE HERE 9
-"""
-monthly = pd.DataFrame(
-  columns = [
-    "January",
-    "February",
-    "March", 
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September", 
-    "October", 
-    "November", 
-    "December"
-    ]
-  )
-"""
-#華氏を摂氏に変える関数を作る。
+#define the function to convert unit.
 def fahr_to_celsius(temp_fahrenheit):
   converted_temp = (temp_fahrenheit - 32) / 1.8
   return converted_temp
 
+#convert data by using defined function
+data['TAVG'] = data['TAVG'].apply(fahr_to_celsius)
 
-#DataFrameを用意する
+#create the DataFrame to answer this part.
 monthly_data = pd.DataFrame()
 
-#dataを加工していく string型にしてsliceで月だけ取得
+# Change data type into a string type and use slice method to get month
 data['TIME_STR'] = data['DATE'].astype(str)
+data['YEAR'] = data['TIME_STR'].str.slice(start=0, stop=4)
 data['MONTH'] = data['TIME_STR'].str.slice(start=4, stop=6)
-data['TAVG'] = data['TAVG'].apply(fahr_to_celsius)
-#'MONTH'の値ごとにグループ化
-grouped = data.groupby('MONTH')
+#'grouped' is grouped data by YEAR and MONTH
+grouped = data.groupby(['YEAR','MONTH'])
 
-#'TAVG'の平均値を取る。
 mean_col = ['TAVG']
+#calculate the average for all group's 'TAVG'
 for key, group in grouped:
-  #平均を計算して、monthly_dataに追加していく。
+  #Calculate the average and add to monthly_data
   mean_values = group[mean_col].mean()
   monthly_data = monthly_data.append(mean_values, ignore_index = True)
 
-#column の名前を正しくする
-new_name = {'TAVG':'temp_celsius'}
-monthly_data = monthly_data.rename(columns = new_name)
-
-
-
-
-#monthly_data = monthly_data.apply(fahr_to_celsius)
-print(monthly_data)
-
+#create the dictionary for changing column name
+new_name = {'TAVG':'temp_celsius'} 
+monthly_data = monthly_data.rename(columns = new_name) 
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print the length of variable monthly_data
